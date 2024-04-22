d85c00e1c7431dc7ecad2e6acfd21337fd3ce517
NiyaziPro <NiyaziPro@users.noreply.github.com>
2024-04-22 11:14:42 +0200

    v2

--- src/Git01.java
+++ src/Git01.java
@@ -3,5 +3,8 @@ public class Git01 {
 
         //  version 1
         System.out.println("Hello Git!");
+
+        // version 2
+        System.out.println("Git init local'de repo baslatmak icin sadece 1 kere kullanilir");
     }
 }
---
+++ src/Git02.java
@@ -0,0 +1,5 @@
+public class Git02 {
+
+    // versiyon 2
+    public String name;
+}

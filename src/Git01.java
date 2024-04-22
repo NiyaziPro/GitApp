eb099763282ed5ff51129ea3bb1b490f7655c068
NiyaziPro <NiyaziPro@users.noreply.github.com>
2024-04-22 11:49:59 +0200

    v4

--- src/Git01.java
+++ src/Git01.java
@@ -9,5 +9,13 @@ public class Git01 {
 
         // version 3
         System.out.println("git log mevcut versiyonlari listeler...");
+
+        // versiyon 4
+        System.out.println("Odev:");
+        System.out.println("Yeni proje olusturalim : GitApp2");
+        System.out.println("Git reposu baslatalim.");
+        System.out.println("Git01,Git02,Git03 class'larini olusturalim.");
+        System.out.println("Main method tanimlayip icinde, sout , if-else ile kodlar yazalim.");
+        System.out.println("Toplamda 6 tane versiyon olusturalim.");
     }
 }
